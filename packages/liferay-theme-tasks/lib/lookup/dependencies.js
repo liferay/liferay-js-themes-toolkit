f08952ab5901f9bd5bc428b86a4bea7f518c7bc2
Julien Castelain <julien.castelain@liferay.com>
2019-03-07 12:59:53 +0100

    Update dependencies for 7.2 themes

--- packages/liferay-theme-tasks/lib/lookup/dependencies.js
+++ packages/liferay-theme-tasks/lib/lookup/dependencies.js
@@ -1,7 +1,7 @@
 function devDependencies(version) {
 	const dependencies = {
 		gulp: '3.9.1',
-		'liferay-theme-tasks': '8.0.0-rc.3',
+		'liferay-theme-tasks': 'file:../packages/liferay-theme-tasks',
 	};
 
 	if (version === '7.0') {
@@ -9,7 +9,11 @@ function devDependencies(version) {
 	} else if (version === '7.1') {
 		dependencies['liferay-theme-deps-7.1'] = '8.0.0-rc.3';
 	} else if (version === '7.2') {
-		dependencies['liferay-theme-deps-7.2'] = '8.0.0-rc.3';
+		dependencies['liferay-theme-deps-7.2'] = 'file:../packages/liferay-theme-deps-7.2';
+		dependencies['css-loader'] = '2.1.0';
+		dependencies['mini-css-extract-plugin'] = '0.5.0';
+		dependencies['postcss-loader'] = '3.0.0';
+		dependencies['sass-loader'] = '7.1.0';
 	}
 
 	return dependencies;
