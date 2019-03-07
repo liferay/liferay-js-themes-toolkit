dd5ec8c9455d2daaab9b580350dc75685a223ee5
Julien Castelain <julien.castelain@liferay.com>
2019-03-07 13:00:10 +0100

    Add temp livereload snippet

--- packages/liferay-theme-tasks/lib/normalize.js
+++ packages/liferay-theme-tasks/lib/normalize.js
@@ -8,7 +8,15 @@
 function normalize(template) {
 	const beforeRegex = /<\/body>/;
 
-	const replacementContent = '<!-- inject:js -->\n<!-- endinject -->\n\n';
+
+	// This should only contain the "script" tag
+	// when the "watch" task is running, for normal deploys we don't
+	// want it.
+	const replacementContent = `
+	<!-- inject:js -->
+		<script src="http://localhost:9080/webpack-dev-server.js"></script>
+	<!-- endinject -->
+	`;
 
 	if (template.indexOf(replacementContent) === -1) {
 		template = template.replace(beforeRegex, function(match) {
